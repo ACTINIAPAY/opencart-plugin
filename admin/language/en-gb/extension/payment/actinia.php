b9731e3616bf3c437f9a899956bd5fbe7ab801c5
selezax <selezax@gmail.com>
2021-09-20 12:54:12 +0300

    INIT

---
+++ admin/language/en-gb/extension/payment/actinia.php
@@ -0,0 +1,36 @@
+<?php
+// Heading
+$_['heading_title']      = 'Actinia';
+$_['text_edit']       = 'Edit module';
+// Text 
+$_['text_payment']       = 'Payment';
+$_['text_actinia']       	 = '<a onclick="window.open(\'http://actinia.eu/\');"><img src="view/image/payment/actinia_logo.png" alt="Actinia" title="Actinia" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_success']       = 'Settings updated';   
+$_['text_pay']           = 'Actinia';
+$_['text_card']          = 'Credit Card';
+$_['text_response_description']               = 'Error text:';
+$_['text_actinia_order_status']                = 'Order status:';
+$_['text_response_code']                      = 'Error code:';
+
+// Entry
+$_['entry_merchant']     = 'Merchant ID:';
+$_['entry_clientcodename']    = 'ClientCodeName:';
+
+
+$_['entry_order_status'] = 'Order status after payment:';
+$_['entry_order_process_status'] = 'Order status in progress:';
+$_['entry_currency']     = 'Currency:';
+$_['entry_language']     = 'Language:';
+$_['entry_help_lang']    =  'Default : ru';
+$_['entry_order_status_cancelled']    =  'Order status in cancelled:';
+$_['entry_geo_zone']	 = 'Geo Zone';
+
+$_['entry_status']       = 'Status:';
+$_['entry_sort_order']   = 'Ordering:';
+$_['entry_styles']		=	'Actinia Styles';
+$_['entry_actinia_result']    =  'Result URL';
+// Error
+$_['error_permission']   = "You haven't permission !";
+$_['error_merchant']     = 'Merchant ID are incorrect!';
+$_['error_clientcodename']    = 'ClientCodeName are empty!';
+?>
---
+++ catalog/language/en-gb/extension/payment/actinia.php
@@ -0,0 +1,12 @@
+<?php
+// Text
+
+$_['text_title'] = 'Actinia VISA/MasterCard';
+$_['error_merchant'] = 'An error has occurred during payment. Merchant data is incorrect.';
+$_['error_signature'] = 'An error has occurred during payment. Signature is not valid.';
+$_['error_transaction'] = 'Transaction has been declined.';
+$_['error_actinia'] = 'Transaction has been declined :';
+$_['error_kod'] = 'Error code :';
+$_['order_desq'] = 'Order pay №';
+$_['order_confirm_pretext'] = 'After confirming the order, you will be directed to the "Actinia" service page for payment by card.';
+?>
